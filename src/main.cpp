3b2b6719f221402246f7102352eab2a7684c5ea6
Buyuk <buyuk.dev@gmail.com>
2014-08-16 02:08:26 +0200

    test program parsing input arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,35 @@
 @file
 @brief: World Engine - Michal Michalski, Lukasz Wojdanowicz
 */
+#include <iostream>
+#include <string>
+
 
 int main( int argc, char* argv[] )
 {
+	const std::string kDefaultInputFileName = "spec.cfg";
+	const std::string kDefaultOutputFileName = "planet.pmd";
 
+	std::string inputFileName = kDefaultInputFileName;
+	std::string outputFileName = kDefaultOutputFileName;
+	switch (argc)
+	{
+	case 1:
+		std::cout << "Using default file names." << std::endl;
+		break;
+	case 2:
+		inputFileName = argv[1];
+		std::cout << "Using default output file name." << std::endl;
+		break;
+	case 3:
+		inputFileName = argv[1];
+		outputFileName = argv[2];
+		break;
+	default:
+		std::cout << "Usage: worldgen <<input>> <<output>>" << std::endl;
+		return -1;
+	}
+	std::cout << "Input file: " << inputFileName << std::endl;
+	std::cout << "Output file: " << outputFileName << std::endl;
 	return 0;
 }
